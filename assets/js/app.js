19b3c915c97c635b113d80a58847b84436c04533
emascapa <emascapa@gmail.com>
2022-04-21 16:24:06 +0200

    milestone 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,35 +4,53 @@ const app = new Vue(
         data: {
             places: [
                 {
-                    image: '../img/01.jpg',
+                    image: './assets/img/01.jpg',
                     title: 'Svezia',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
                 },
                 {
-                    image: '../img/02.jpg',
+                    image: './assets/img/02.jpg',
                     title: 'Svizzera',
                     text: 'Lorem ipsum.',
                 },
                 {
-                    image: '../img/03.jpg',
+                    image: './assets/img/03.jpg',
                     title: 'Gran Bretagna',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
                 },
                 {
-                    image: '../img/04.jpg',
+                    image: './assets/img/04.jpg',
                     title: 'Germania',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
                 },
                 {
-                    image: '../img/05.jpg',
+                    image: './assets/img/05.jpg',
                     title: 'Paradise',
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
                 }
             ],
+            img_counter: 0,
 
-            
-            active_img: 'border border-3 border-primary',
+
+            active_img: 'border border-4 border-info',
         },
-        methods: {}
+        methods: {
+            buttonUp() {
+                console.log('ciaoooo');
+                if (this.img_counter === 0) {
+                    this.img_counter = this.places.length - 1
+                } else {
+                    this.img_counter--
+                }
+            },
+            buttonDown() {
+                console.log('arrivederciiii');
+                if (this.img_counter === this.places.length - 1) {
+                    this.img_counter = 0
+                } else {
+                    this.img_counter++
+                }
+            },
+        }
     }
 )
